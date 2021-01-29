599d1b15d7d921b80a31f2ce3524c2e64ca1dc64
jiannei <longjian.huang@aliyun.com>
2021-01-29 18:34:40 +0800

    pref: 路由调整

--- routes/web.php
+++ routes/web.php
@@ -1,39 +1,50 @@
 <?php
 
+use App\Http\Controllers\Auth\ForgotPasswordController;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Auth\ResetPasswordController;
+use App\Http\Controllers\Auth\VerificationController;
+use App\Http\Controllers\CategoriesController;
+use App\Http\Controllers\NotificationsController;
+use App\Http\Controllers\PagesController;
+use App\Http\Controllers\RepliesController;
+use App\Http\Controllers\TopicsController;
+use App\Http\Controllers\UsersController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', 'TopicsController@index')->name('root');
+Route::get('/', [TopicsController::class, 'index'])->name('root');
 
 // 用户身份验证相关的路由
-Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
-Route::post('login', 'Auth\LoginController@login');
-Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+Route::get('login', [LoginController::class, 'showLoginForm'])->name('login');
+Route::post('login', [LoginController::class, 'login']);
+Route::post('logout', [LoginController::class, 'logout'])->name('logout');
 
 // 用户注册相关路由
-Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
-Route::post('register', 'Auth\RegisterController@register');
+Route::get('register', [RegisterController::class, 'showRegistrationForm'])->name('register');
+Route::post('register', [RegisterController::class, 'register']);
 
 // 密码重置相关路由
-Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
-Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
-Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
-Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');
+Route::get('password/reset', [ForgotPasswordController::class, 'showLinkRequestForm'])->name('password.request');
+Route::post('password/email', [ForgotPasswordController::class, 'sendResetLinkEmail'])->name('password.email');
+Route::get('password/reset/{token}', [ResetPasswordController::class, 'showResetForm'])->name('password.reset');
+Route::post('password/reset', [ResetPasswordController::class, 'reset'])->name('password.update');
 
 // Email 认证相关路由
-Route::get('email/verify', 'Auth\VerificationController@show')->name('verification.notice');
-Route::get('email/verify/{id}/{hash}', 'Auth\VerificationController@verify')->name('verification.verify');
-Route::post('email/resend', 'Auth\VerificationController@resend')->name('verification.resend');
+Route::get('email/verify', [VerificationController::class, 'show'])->name('verification.notice');
+Route::get('email/verify/{id}/{hash}', [VerificationController::class, 'verify'])->name('verification.verify');
+Route::post('email/resend', [VerificationController::class, 'resend'])->name('verification.resend');
 
-Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
+Route::resource('users', UsersController::class, ['only' => ['show', 'update', 'edit']]);
 
-Route::resource('topics', 'TopicsController', ['only' => ['index', 'create', 'store', 'update', 'edit', 'destroy']]);
-Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
+Route::resource('topics', TopicsController::class, ['only' => ['index', 'create', 'store', 'update', 'edit', 'destroy']]);
+Route::resource('categories', CategoriesController::class, ['only' => ['show']]);
 
-Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload_image');
+Route::post('upload_image', [TopicsController::class, 'uploadImage'])->name('topics.upload_image');
 
-Route::get('topics/{topic}/{slug?}', 'TopicsController@show')->name('topics.show');
+Route::get('topics/{topic}/{slug?}', [TopicsController::class, 'show'])->name('topics.show');
 
-Route::resource('replies', 'RepliesController', ['only' => ['store', 'destroy']]);
+Route::resource('replies', RepliesController::class, ['only' => ['store', 'destroy']]);
 
-Route::resource('notifications', 'NotificationsController', ['only' => ['index']]);
-Route::get('permission-denied', 'PagesController@permissionDenied')->name('permission-denied');
+Route::resource('notifications', NotificationsController::class, ['only' => ['index']]);
+Route::get('permission-denied', [PagesController::class, 'permissionDenied'])->name('permission-denied');
